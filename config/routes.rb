69f8005d8f9eaaf5b5e069ad270e7f9534d79d87
Yana Petrova <yana.petrova@distribusion.com>
2026-03-22 17:42:57 +0100

    fix style

--- config/routes.rb
+++ config/routes.rb
@@ -37,5 +37,5 @@ Rails.application.routes.draw do
 
   root to: 'recipes#index'
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
